4a7c2525c8f7a1fb9dbfb5fb527eb284156d4da8
William <110736589+ConnorMurphy9@users.noreply.github.com>
2023-04-12 19:06:11 -0400

    fixed sequelize error

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const path = require('path')
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3006;
 const seedAll = require('./seeds/index')
 
 const sess = {
