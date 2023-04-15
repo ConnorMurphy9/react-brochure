7f4369253bf6a33b23aea9d77f8bb83864455c09
William <110736589+ConnorMurphy9@users.noreply.github.com>
2023-04-14 23:57:07 -0400

    fixing authentication for mysql

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,21 +1,16 @@
 const Sequelize = require("sequelize");
 let sequelize;
-require("dotenv").config();
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
+
+{
   sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASS,
     {
       host: "us-cdbr-east-06.cleardb.net",
+      user: "bb717135cfa3c5",
+      password: "c1327801",
       dialect: "mysql",
       port: 3306,
-      database: "heroku_718cc1d874f1b03"
+      database: "heroku_1c65d7b327446ef"
     }
   );
 }
 module.exports = sequelize;
-
-// mysql://bb1d127f81b6e3:a83d4adf@us-cdbr-east-06.cleardb.net/heroku_718cc1d874f1b03?reconnect=true
