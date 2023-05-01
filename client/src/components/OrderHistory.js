64bb50861b19822354501469e85671c04582a614
William <110736589+ConnorMurphy9@users.noreply.github.com>
2023-05-01 16:00:51 -0400

    troubleshooting firebase import error

--- client/src/components/OrderHistory.js
+++ client/src/components/OrderHistory.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import firebase from "firebase";
+import firebase from "firebase/compat/app";
 import "firebase/firestore";
 
 const OrderHistory = () => {
