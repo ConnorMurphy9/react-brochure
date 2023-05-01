d44784d179d498f27b45a84748b54ef8fc7f7ed2
William <110736589+ConnorMurphy9@users.noreply.github.com>
2023-05-01 16:13:25 -0400

    fixed server file names

--- server/server.js
+++ server/server.js
@@ -1,54 +1,87 @@
-const express = require('express');
-const routes = require('./controllers');
-const sequelize = require('./config/connection');
-const session = require('express-session');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-const path = require('path')
-const app = express();
-const PORT = process.env.PORT || 3001;
+const express = require("express");
+const bodyParser = require("body-parser");
+const mysql = require("mysql2");
+const { Sequelize } = require("sequelize");
+const admin = require("firebase-admin");
 const seedAll = require('./seeds/index')
 
+const app = express();
+const port = process.env.PORT || 5000;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-const sess = {
-  secret: 'Super duper top secret',
-  cookie: {
-      maxAge: 24 * 60 * 60 * 1000,
-      httpOnly: true,
-      secure: false,
-      sameSite: 'strict',
-  },
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-      db: sequelize
-  }),
-}
-
-app.use(session(sess))
-
-
-
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// turn on routes
-app.use(routes);
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// Firebase configuration
+const firebaseServiceAccount = require("./serviceAccountKey.json");
+admin.initializeApp({
+  credential: admin.credential.cert(firebaseServiceAccount),
+  databaseURL: "https://auth-brochure-development.firebaseio.com",
 });
 
+const firebaseAuthMiddleware = async (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  if (!authHeader) {
+    return res.sendStatus(401);
+  }
+
+  const token = authHeader.split(" ")[1];
+  try {
+    const decodedToken = await admin.auth().verifyIdToken(token);
+    req.user = decodedToken;
+    next();
+  } catch (error) {
+    console.error(error);
+    return res.sendStatus(401);
+  }
+};
+
+const sequelize = new Sequelize("pizzeria_db", "root", "root123", {
+  host: "localhost",
+  dialect: "mysql",
+});
+
+sequelize
+  .authenticate()
+  .then(() => console.log("Database connected!"))
+  .catch((err) => console.error("Error connecting to database: ", err));
+
+const User = sequelize.define("user", {
+  firebase_id: Sequelize.STRING,
+});
+
+const Pizza = sequelize.define("pizza", {
+  name: Sequelize.STRING,
+  description: Sequelize.TEXT,
+  price: Sequelize.FLOAT,
+  image_url: Sequelize.STRING,
+});
+
+User.hasMany(Pizza);
+Pizza.belongsTo(User);
+
+app.get("/api/pizzas", async (req, res) => {
+  const pizzas = await Pizza.findAll();
+  res.send(pizzas);
+});
+
+app.post("/api/pizzas", firebaseAuthMiddleware, async (req, res) => {
+  const { name, description, price, image_url } = req.body;
+  const user = await User.findOne({ where: { firebase_id: req.user.uid } });
+  const pizza = await user.createPizza({
+    name,
+    description,
+    price,
+    image_url,
+  });
+  res.send(pizza);
+});
+
+// app.listen(port, () => console.log(`Server running on port ${port}`));
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(async () => {
-  await seedAll()
-  await app.listen(PORT, () => {
-      console.log(`App Listening on port ${PORT}`)
-  });
-})
-
+    await seedAll()
+    await app.listen(port, () => {
+        console.log(`App Listening on port ${port}`)
+    });
+  })
